2250b1e73ca1ab4285fa672c2006ea9aa70ae059
Remi Lejeune <remi@lejeunebernot.net>
2024-06-16 20:34:29 +0200

    Add predict video

--- predict_video.py
+++ predict_video.py
@@ -1,44 +1,45 @@
-import numpy
 from ultralytics import YOLO
 from ultralytics import RTDETR
 import random
+import cv2
 import torch
-from ultralytics.utils.benchmarks import benchmark
+import time
 
-def calc_f1(precision, recall):
-    return 2 * (recall * precision) / (recall + precision)
 
 if __name__ == '__main__':
     random.seed(10)
-    # print(calc_f1(0.881, 0.565))
     # print(f'CUDA version: {torch.version.cuda}')
     # cuda_id = torch.cuda.current_device()
     # print(f"ID of current CUDA device:{torch.cuda.current_device()}")
     # print(f"Name of current CUDA device:{torch.cuda.get_device_name(cuda_id)}")
     # print(f'Torch version: {torch.__version__}')
-    choice = input("1=yolo, 2=fastsam, 3=rtdetr")
-    data = "C:/Users/Justin/Desktop/CV_DL/data/Santyasa.v4-tester-3.yolov9/data.yaml"
+
+
+
+    # choice = input("1=yolo, 2=fastsam, 3=rtdetr")
+    choice = '3'
     if choice == '1':
         chosen_model = "weights/best_weights_yolo_final.pt"
         model = YOLO(chosen_model)
     elif choice == '2':
         chosen_model = "weights/best_weights_fastsam_final.pt"
-        data = "C:/Users/Justin/Desktop/CV_DL/data/yolo_seg_data/cv/data.yaml"
         model = YOLO(chosen_model)
     else:
         chosen_model = "weights/best_weights_rtdetr_final.pt"
         model = RTDETR(chosen_model)
-    choice = input("edge(=1) or not(=2)")
-    if choice == '1':
-        if chosen_model == 'weights/best_weights_fastsam_final.pt':
-            data = "C:/Users/Justin/Desktop/CV_DL/data/edge_cases_segments/data.yaml"
-        else:
-            data = "C:/Users/Justin/Desktop/CV_DL/data/edge_cases/data.yaml"
 
-    # metrics = model.val(conf=0.5, data=data, split="test")
 
     video_path = "arj_cv_vid.mp4"
     cap = cv2.VideoCapture(video_path)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    output_video = cv2.VideoWriter('output_video.mp4', fourcc, fps, (width, height))
+
+    inference_times = []
 
     # Loop through the video frames
     while cap.isOpened():
@@ -47,11 +48,19 @@ if __name__ == '__main__':
 
         if success:
             # Run YOLOv8 tracking on the frame, persisting tracks between frames
-            results = model.track(frame, persist=True, conf=0.65)
+
+            start_time = time.time()
+
+            results = model.track(frame, persist=True, conf=0.5)
+
+            end_time = time.time()
+
+            inference_time = end_time - start_time
+            inference_times.append(inference_time)
 
             # Visualize the results on the frame
             annotated_frame = results[0].plot()
-
+            output_video.write(annotated_frame)
             # Display the annotated frame
             cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
@@ -64,4 +73,8 @@ if __name__ == '__main__':
 
     # Release the video capture object and close the display window
     cap.release()
+    output_video.release()
     cv2.destroyAllWindows()
+
+    avg_inference_time = sum(inference_times) / len(inference_times) if inference_times else 0
+    print(f'Average inference time: {avg_inference_time:.4f} seconds')
